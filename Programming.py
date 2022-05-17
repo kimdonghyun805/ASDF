fa6b8725501304f77513c5ffc5dd0dc3eb97c706
kdh <s6580859@naver.com>
2022-05-17 21:02:09 +0900

    Programming.py
    생성한 위젯을 관리, 기능 수행

---
+++ Programming.py
@@ -0,0 +1,28 @@
+import Header
+
+class Programming(Header.QWidget) :
+    def __init__(self, size_x, size_y, dictionary_icon, font, widget) :
+        super().__init__(widget)
+        self.size_x = size_x
+        self.size_y = size_y
+        
+        self.icon_move = dictionary_icon["move"]
+        self.icon_edit = dictionary_icon["edit"]
+        self.icon_close = dictionary_icon["close"]
+
+        self.font = font
+        print("programming font :", self.font.toString())
+
+        self.resize(Header.QSize(self.size_x, self.size_y))
+        self.setFixedSize(self.size_x, self.size_y)
+
+        self.widget = Header.QWidget(self)
+        self.widget.resize(Header.QSize(self.size_x - 20, self.size_y - 20))
+        self.setFixedSize(self.size_x - 20, self.size_y - 20)
+        self.widget.move(0, 0)
+
+        self.widget.setStyleSheet("border-style : solid; border-width : 2px; border-color : #00FF00;")
+        self.title = Header.QLabel("Programming", self.widget)
+        self.title.setFont(self.font)
+        self.title.move(10, 10)
+

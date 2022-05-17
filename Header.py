6498188f4872f8732fc665365831c0aa06bc1f6d
kdh <s6580859@naver.com>
2022-05-17 21:02:57 +0900

    Header.py
    필요한 라이브러리를 모아놓음

---
+++ Header.py
@@ -0,0 +1,11 @@
+import sys
+import os
+import pickle
+import copy
+
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QDesktopWidget, 
+                             QAction, QToolBar, QVBoxLayout, QScrollArea,
+                             QPushButton, QDialog, QLabel, QTextEdit, QLineEdit, 
+                             QGroupBox, QGridLayout, QHBoxLayout)
+from PyQt5.QtGui import (QIcon, QFont, QFontDatabase, QIntValidator)
+from PyQt5.QtCore import (QSize, Qt, pyqtSignal, QThread, QPoint)
